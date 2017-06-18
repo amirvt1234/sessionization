2aa2b0eab93f1702571dbf17bd29103757931b1f
Ubuntu <ubuntu@ip-172-31-53-147.ec2.internal>
2017-06-18 23:34:48 +0000

    some work on producer

--- consumer/src/main/java/consumer/Windows.java
+++ consumer/src/main/java/consumer/Windows.java
@@ -16,6 +16,8 @@ import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.util.Collector;
 import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows;
 import org.apache.flink.streaming.api.windowing.assigners.ProcessingTimeSessionWindows;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.functions.TimestampExtractor;
 import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
 import org.apache.flink.api.common.functions.ReduceFunction;
@@ -32,22 +34,30 @@ import com.typesafe.config.Config;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.*;
 import java.util.Iterator;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import org.apache.flink.api.java.utils.ParameterTool;
 
 
-public class Windows {
 
-	public static int SPIDERSN;
+public class Windows  {
+
+	public static String propertiesFile = "../myjob.properties";
+	
+
+	public static int SPIDERSN=90; 
+	public static int TWINDOWS=60;
 
     public static void main(String[] args) throws Exception {
-
+		ParameterTool parameter = ParameterTool.fromPropertiesFile(propertiesFile);
 		// Set up the flink streaming environment
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
 
 		// Reading configureations
 		// Find a way to print an error message while there is an IO error...
@@ -59,8 +69,9 @@ public class Windows {
 		String MASTERIP  = (String) jsonObject.get("MASTER_IP");
 		String TOPIC     = (String) jsonObject.get("TOPIC");
 		Integer TUMBLINGW = Integer.parseInt((String) jsonObject.get("WINDOW_SIZE_T")); 
-		Integer SESSIONWS = Integer.parseInt((String) jsonObject.get("WINDOW_SIZE_S")); 
-		SPIDERSN  = Integer.parseInt((String) jsonObject.get("SPIDERSN"));
+		Integer SESSIONWS = Integer.parseInt((String) jsonObject.get("WINDOW_SIZE_S"));
+		String REDISIP = (String) jsonObject.get("REDIS_IP"); 
+
 
 		// Kafka connector
         Properties properties = new Properties();
@@ -80,10 +91,20 @@ public class Windows {
             .timeWindow(Time.seconds(TUMBLINGW))
             .sum(3);
 
+		//The totalcount will perform on a single machine by nature
+		//What I have done here is not efficient. This part needs to be done in the 
+		// database environment to avoid repeating the same counting we did with groupby
+		// Left it for knwo for testing purposes... 
+        DataStream<Tuple4<String, Long, Long, Integer>> totalcount = datain
+            //.windowAll(TumblingEventTimeWindows.of(Time.seconds(60)))
+			.windowAll(TumblingProcessingTimeWindows.of(Time.seconds(TUMBLINGW)))
+            .sum(3);
+
 		// If the number of clicks during the summed window (clickcount) is larger
 		// than the value specified at SPIDERSN (myconfig.json) classify then as spiders(machine web crawlers)
 		SplitStream<Tuple4<String, Long, Long, Integer>> detectspider = clickcount
 				.split(new SpiderSelector());
+
 
 		DataStream<Tuple4<String, Long, Long, Integer>> spiders = detectspider.select("spider");
 
@@ -99,6 +120,7 @@ public class Windows {
         });
 
 
+		// move reduce function inside a new class...
         DataStream<Tuple4<String, Long, Long, Integer>> testsession = withTimestampsAndWatermarks
             .keyBy(0)
             //.window(EventTimeSessionWindows.withGap(Time.milliseconds(2L)))
@@ -112,23 +134,25 @@ public class Windows {
 
 
 
-        FlinkJedisPoolConfig redisConf = new FlinkJedisPoolConfig.Builder().setHost("127.0.0.1").setPort(6379).build();
+        FlinkJedisPoolConfig redisConf = new FlinkJedisPoolConfig.Builder().setHost(REDISIP).setPort(6379).build();
 
-		spiders.addSink(new RedisSink<Tuple4<String, Long, Long, Integer>>(redisConf, new ViewerCountMapper()));
-
-
-//	FlinkJedisPoolConfig redisConf = new FlinkJedisPoolConfig.Builder().setHost("172.31.53.147").setPort(6379).build();
-//        FlinkJedisPoolConfig redisConf = new FlinkJedisPoolConfig.Builder().setHost("127.0.0.1").setPort(6379).build();
-//	clickcount.addSink(new RedisSink<Tuple4<String, Long, Long, Integer>>(redisConf, new ViewerCountMapper()));
-        clickcount.print();
+		clickcount.addSink(new RedisSink<Tuple4<String, Long, Long, Integer>>(redisConf, new ViewerCountMapper()));
+		totalcount.addSink(new RedisSink<Tuple4<String, Long, Long, Integer>>(redisConf, new TotalCountMapper()));
+		//totalcount.addSink(new RedisSink<Tuple4<String, Long, Long, Integer>>(redisConf, new SpidersID()));
+		detectspider
+		    .select("spider")
+			.addSink(new RedisSink<Tuple4<String, Long, Long, Integer>>(redisConf, new SpidersIDMapper()));
+        //clickcount.print();
+		totalcount.print();
         //testsession.print();
 	//	spiders.print();
 
 
-        env.execute("Window WordCount");
+        env.execute("Sessionization");
 
 
     }
+
 
 	// figure out what does "serialVersionUID" means. Many codes include it but dont know why....
 	public static class SpiderSelector implements OutputSelector<Tuple4<String, Long, Long, Integer>> {
@@ -137,7 +161,10 @@ public class Windows {
 		@Override
 		public Iterable<String> select(Tuple4<String, Long, Long, Integer> value) {
 			List<String> output = new ArrayList<>();
+			
 			if (value.f3 > SPIDERSN) {
+			//if (value.f3 > SESSIONWSdd){
+			//if (value.f3 > 80) {
 				output.add("spider");
 			} else {
 				output.add("legit");
@@ -145,9 +172,6 @@ public class Windows {
 			return output;
 		}
 	}
-
-
-
 
     public static class LineSplitter implements FlatMapFunction<String, Tuple4<String, Long, Long, Integer>> {
         @Override
@@ -157,28 +181,28 @@ public class Windows {
         }
     }
 
-
-	public static class ViewerCountMapper implements RedisMapper<Tuple4<String, Long, Long, Integer>>{
+	public static class EngagementMapper implements RedisMapper<Tuple4<String, Long, Long, Integer>> {
 
 		@Override
 		public RedisCommandDescription getCommandDescription() {
-		return new RedisCommandDescription(RedisCommand.HSET, "HASH_NAME");
+			return new RedisCommandDescription(RedisCommand.ZADD, "EngagementTime");
 		}
 
 		@Override
 		public String getKeyFromData(Tuple4<String, Long, Long, Integer> data) {
-		return data.f0;
+			return data.getField(0);
 		}
 
 		@Override
 		public String getValueFromData(Tuple4<String, Long, Long, Integer> data) {
-		return data.f1.toString();
+			Double stime =  ((Long) data.getField(1)).doubleValue();			
+			Double etime =  ((Long) data.getField(2)).doubleValue();
+			//fixme
+			return Double.toString(Math.floor(etime-stime));
 		}
 	}
 
-
-	/*
-	public class ViewerCountMapper implements RedisMapper<Tuple4<String, Long, Long, Integer>> {
+	public static class ViewerCountMapper implements RedisMapper<Tuple4<String, Long, Long, Integer>> {
 
 		@Override
 		public RedisCommandDescription getCommandDescription() {
@@ -195,196 +219,45 @@ public class Windows {
 			return data.getField(3).toString();
 		}
 	}
-	*/
 
 
 
+	public static class TotalCountMapper implements RedisMapper<Tuple4<String, Long, Long, Integer>>{
 
-//        FlinkKafkaProducer09<String> myProducer = new FlinkKafkaProducer09<String>(
-//                "localhost:9092",            // broker list
-//                "jtestc",                  // target topic
-//                new SimpleStringSchema());   // serialization schema
+		@Override
+		public RedisCommandDescription getCommandDescription() {
+		return new RedisCommandDescription(RedisCommand.HSET, "TOTAL_COUNT");
+		}
 
-//		        StringKafka.addSink(myProducer);
-//        DataStream<String> StringKafka = aggregated.map(new TupleToStr());
+		@Override
+		public String getKeyFromData(Tuple4<String, Long, Long, Integer> data) {
+		return (String) "totalcount";
+		}
 
-/*
-    public static class TupleToStr implements MapFunction<Tuple3<String, Long, Integer>, String> {
-      @Override
-      public String map(Tuple3<String, Long, Integer> in) {
-        return in.f0 + ';' + in.f1.toString() + ';' + in.f2.toString();
-      }
-    }
-*/
+		@Override
+		public String getValueFromData(Tuple4<String, Long, Long, Integer> data) {
+		return data.f3.toString();
+		}
+	}
+
+	public static class SpidersIDMapper implements RedisMapper<Tuple4<String, Long, Long, Integer>>{
+
+		@Override
+		public RedisCommandDescription getCommandDescription() {
+		return new RedisCommandDescription(RedisCommand.HSET, "SPIDERS");
+		}
+
+		@Override
+		public String getKeyFromData(Tuple4<String, Long, Long, Integer> data) {
+		return data.f0;
+		}
+
+		@Override
+		public String getValueFromData(Tuple4<String, Long, Long, Integer> data) {
+		return (String) "True";
+		}
+	}
+
 
 }
 
-
-
-
-
-
-/*
-
-import org.apache.flink.api.common.functions.FoldFunction;
-import org.apache.flink.api.java.functions.KeySelector;
-import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.datastream.KeyedStream;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditEvent;
-import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditsSource;
-
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer09;
-import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
-import org.apache.flink.api.common.functions.MapFunction;
-
-//import org.apache.flink.streaming.api.datastream.DataStream;
-//import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-//import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
-//import org.apache.flink.streaming.api.windowing.time.Time;
-//import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
-
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer09;
-//import java.text.SimpleDateFormat;
-import java.util.*;
-import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
-//import org.json.JSONObject;
-//import org.apache.flink.streaming.connectors.cassandra.CassandraSink;
-//import com.datastax.driver.core.Cluster;
-//import com.typesafe.config.ConfigFactory;
-
-import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.api.java.tuple.Tuple3;
-import org.apache.flink.api.java.tuple.Tuple4;
-import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.util.Collector;
-
-
-//////////
-import org.apache.flink.api.java.utils.ParameterTool;
-import org.apache.flink.streaming.api.TimeCharacteristic;
-import org.apache.flink.streaming.api.functions.source.SourceFunction;
-import org.apache.flink.streaming.api.watermark.Watermark;
-//import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows;
-//import org.apache.flink.streaming.api.windowing.assigners;
-import org.apache.flink.streaming.api.windowing.time.Time;
-
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer09;
-import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.common.functions.MapFunction;
-
-import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
-import org.apache.flink.util.Collector;
-import org.apache.flink.streaming.api.windowing.windows.Window;
-import org.apache.flink.api.java.functions.KeySelector;
-
-import org.apache.flink.api.common.functions.ReduceFunction;
-//////////
-
-
-import org.apache.flink.api.common.functions.ReduceFunction;
-import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows;
-import org.apache.flink.streaming.api.windowing.assigners.ProcessingTimeSessionWindows;
-import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
-import org.apache.flink.streaming.api.windowing.windows.Window;
-import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
-
-
-//import com.typesafe.config.Config;
-//import org.apache.flink.api.common.functions.FlatMapFunction;
-//import org.apache.flink.api.common.functions.FoldFunction;
-//import org.apache.flink.api.java.functions.KeySelector;
-//import org.apache.flink.api.java.tuple.Tuple4;
-//import org.apache.flink.api.java.tuple.Tuple7;
-import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
-import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
-//import org.apache.flink.streaming.connectors.cassandra.ClusterBuilder;
-//import org.apache.flink.streaming.connectors.redis.RedisSink;
-//import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig;
-//import org.apache.flink.util.Collector;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer09;
-//import java.text.SimpleDateFormat;
-import java.util.*;
-import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
-//import org.json.JSONObject;
-//import org.apache.flink.streaming.connectors.cassandra.CassandraSink;
-//import com.datastax.driver.core.Cluster;
-//import com.typesafe.config.ConfigFactory;
-
-import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.api.java.tuple.Tuple3;
-import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.util.Collector;
-
-
-import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows;
-import org.apache.flink.streaming.api.windowing.assigners.ProcessingTimeSessionWindows;
-
-import org.apache.flink.streaming.api.functions.TimestampExtractor;
-
-//import org.apache.flink.streaming.api.functions.AscendingTimestampExtractor;
-
-import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
-
-
-import org.apache.flink.api.common.functions.ReduceFunction;
-import org.apache.flink.streaming.api.watermark.Watermark;
-
-
-
-
-public class WikipediaAnalysis {
-
-  public static void main(String[] args) throws Exception {
-
-    StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-    Properties properties = new Properties();
-    properties.setProperty("bootstrap.servers", "ec2-52-203-199-182.compute-1.amazonaws.com:9092");
-        // only required for Kafka 0.8
-    properties.setProperty("zookeeper.connect", "ec2-52-203-199-182.compute-1.amazonaws.com:2181");
-    properties.setProperty("group.id", "test");
-        //DataStream<String> dataStream = env
-    DataStream<Tuple4<String, Long, Long, Integer>> datain = env
-        .addSource(new FlinkKafkaConsumer09<>("jtest", new SimpleStringSchema(), properties))
-        .flatMap(new LineSplitter());
-
-    DataStream<Tuple4<String, Long, Long, Integer>> clickcount = datain
-        .keyBy(0)
-        .timeWindow(Time.seconds(1))
-        .sum(3);
-
-	DataStream<Tuple4<String, Long, Long, Integer>> usersession = datain
-				.window(EventTimeSessionWindows.withGap(Time.milliseconds(60000L)));
-//                .reduce (new ReduceFunction<Tuple4<String, Long, Long, Integer>>() {
-//                    public Tuple4<String, Long, Long, Integer> reduce(Tuple4<String, Long, Long, Integer> value1, Tuple4<String, Long, Long, Integer> value2) throws Exception {
-//                        return new Tuple4<String, Long, Long, Integer>(value1.f0, value1.f1, value2.f1, value1.f3+value2.f3);
-//                    }
-//                });
-
-	usersession.print();
-	env.execute("Window WordCount");
-  }
-
-    public static class LineSplitter implements FlatMapFunction<String, Tuple4<String, Long, Long, Integer>> {
-        @Override
-        public void flatMap(String line, Collector<Tuple4<String, Long, Long, Integer>> out) {
-            String[] word = line.split(";");
-            out.collect(new Tuple4<String, Long, Long, Integer>(word[0], Long.parseLong(word[1]), Long.parseLong(word[1]), 1));
-        }
-    }
-
-}
-
-*/
--- producer/pykafka_producer.py
+++ producer/pykafka_producer.py
@@ -51,14 +51,54 @@ with topic.get_producer(partitioner=hash_partitioner, linger_ms = 200) as produc
 		totallogs += len(ld1)
 
 """
-ld1 = []
-for i in range(30):
-	ld1.append([i,1]) 
-with topic.get_producer(partitioner=hash_partitioner, linger_ms = 200) as producer:
+
+def unittestproducer():
+	tw = 1*60. # The time window in seconds
+	#visitg = [1,2,10,100]
+	visitg = [1, 2, 10, 1000] 
+	nvfv = {'1'   :[0       , int(10), int(5)], # [0,   1e6): (forgetters)
+		'2'   :[int(10), int(20), int(5)], # [1e6, 2e6): (login-logout) 
+		'10'  :[int(20), int(30), int(3)], # [2e6, 3e6): (active-user)
+		'1000' :[int(30), int(40), int(100)]} # [3e6, 4e6): (machine-spam)
+	dtime = 0.
+	nv = 0 # Initiate the number of the visits to the website during the time window
+	ld = []
+	for v in visitg:
+		nv += v*nvfv[str(v)][2]
+		ld.append(np.repeat(np.random.randint(nvfv[str(v)][0], nvfv[str(v)][1], size=nvfv[str(v)][2]), v)) 
+
+	ld1 = np.concatenate(ld[:])
+	np.random.shuffle(ld1)
+	#dt = tw/len(ld1)
+	#eventTime = 0.
+	dt = tw/len(ld1)
+	eventTime = 0
+	delay = 0
+	#times = time.time()
 	for index, item in enumerate(ld1):
-		##outputStr = "%s;%i".format(str(item[0]), item[1])
-		outputStr = b"{};{}".format(item[0], item[1])
-		producer.produce(outputStr, partition_key="%i".format(item[0]))
+		times = time.time()
+		eventTime  += dt-delay # event time in Seconds
+		currID = item
+	    	#outputStr = "%s;%s" % (currID, eventTime+dtime)
+        	outputStr = "{};{}".format(currID, np.int(1))
+		#times = time.time()
+	    	producer.produce(outputStr, partition_key=str(currID))
+	    	delay = (time.time()-times)
+		#if (dt-delay)<0.:
+		#	print "hey fix me"
+                #print dt, delay
+		time.sleep(max(0.7*(dt-delay),1e-9))
+		times = time.time()
+
+#ld1 = []
+#for i in range(30):
+#	ld1.append([i,1]) 
+with topic.get_producer(partitioner=hash_partitioner, linger_ms = 200) as producer:
+	unittestproducer()
+	#for index, item in enumerate(ld1):
+	#	##outputStr = "%s;%i".format(str(item[0]), item[1])
+	#	outputStr = b"{};{}".format(item[0], item[1])
+	#	producer.produce(outputStr, partition_key="%i".format(item[0]))
 
 
 
