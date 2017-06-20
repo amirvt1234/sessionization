4454d55eca2820f7cd1132339ee41782d0a2502e
Ubuntu <ubuntu@ip-172-31-53-147.ec2.internal>
2017-06-20 12:23:36 +0000

    more frontend

--- frontend/app/dashboard.py
+++ frontend/app/dashboard.py
@@ -1,6 +1,8 @@
 import redis
 import json
 from flask import jsonify
+#from flask_table import Table
+from flask_table import Table, Col
 
 
 with open('../../myconfigs.json', 'r') as f:
@@ -27,21 +29,38 @@ def get_spiders():
     #jsonredis = [{"name": x[0]}]
     return fromredis
 
-ss = get_most_viewed()
-print ss, type(ss)
-spider = get_spiders()
-print spider, type(spider)
-ss = get_total_count()
-print ss, type(ss)
-ss = get_most_engaged()
-print ss, type(ss)
+most_viewed = get_most_viewed()
+#print ss, type(ss)
+#spider = get_spiders()
+#print spider, type(spider)
+#ss = get_total_count()
+#print ss, type(ss)
+#ss = get_most_engaged()
+#print ss, type(ss)
+
+# Declare your table
+class ItemTable(Table):
+    name = Col('Name')
+    description = Col('Description')
+
+items = [dict(name='Name1', description='Description1'),
+         dict(name='Name2', description='Description2'),
+         dict(name='Name3', description='Description3')]
+
+
+dummy = [] 
+for i in most_viewed:
+    print i
+    dummy.append(dict(name=i[0], description=str(i[1])))
+print dummy[0], type(dummy[0])
+print items[0], type(items[0])
 
 
 
-if not spider['39']:
-    print "good"
-else:
-    print 'bad'
+#if not spider['39']:
+#    print "good"
+#else:
+#    print 'bad'
 
 """
 ss = get_spiders()
--- frontend/app/views.py
+++ frontend/app/views.py
@@ -2,17 +2,52 @@ from app import app
 from flask import render_template
 import json
 import redis
+# import things
+from flask_table import Table, Col
+import flask
+#from bokeh.embed import components
+from bokeh.plotting import figure
+#from bokeh.resources import INLINE
+#from bokeh.templates import RESOURCES
+#from bokeh.util.string import encode_utf8
+
 
 with open('../myconfigs.json', 'r') as f:
     myconfigs = json.load(f)
 redisConnection = redis.StrictRedis(host="172.31.53.147", port=6379, db=0)
 
+# Declare your table
+class mostviewedtable(Table):
+    name = Col('User ID')
+    description = Col('Number of Clicks')
+
+class mostengagedtabe(Table):
+    name = Col('User ID')
+    description = Col('Session Time')
+
+
+
 def get_most_engaged():
-       response = redisConnection.zrange('EngagementTime',1,10,True,True)
-       return response
+       response = redisConnection.zrange('EngagementTime',0,10,True,True)
+       return [dict(name=x[0], description=str(int(x[1]/1000))) for x in response]
+
+def get_most_viewed():
+       response = redisConnection.zrange('ViewerCount',0,10,True,True)
+       return [dict(name=x[0], description=str(int(x[1]))) for x in response]
+
+
+most_viewed = get_most_viewed()
+most_engaged = get_most_engaged()
+
+#10_most_engaged = get_most_engaged()
 
 @app.route('/')
 @app.route('/index')
 def index():
-  user = { 'nickname': 'Miguelaaaaasdf' } # fake user
-  return render_template("index.html", title = 'Home', user = user)
+  user = { 'nickname': 'This is my UI!' } # fake user
+  table1 = mostviewedtable(most_viewed)
+  table2 = mostengagedtabe(most_engaged)
+  #tables = [mostviewedtable(most_viewed), mostengagedtabe(most_engaged)]
+  titles = ['na', 'Most Viewed', 'Most Engaged']
+  return render_template("index.html", title = 'Home', user = user,  tables=[table1, table2], titles=titles)
+
